064738da8accf665be4bd61d5554eb1ba670598c
Ashton Yeackley <aey21@fsu.edu>
2022-11-28 13:36:34 -0500

    added js files and scene manager

--- sketch.js
+++ sketch.js
@@ -5,3 +5,8 @@ function setup() {
 function draw() {
   background(125);
 }
+
+
+// https://tree-nation.com/projects/inside-tree-nation/article/9739-how-many-trees-are-lost-to-deforestation-every-year
+// https://www.condorferries.co.uk/marine-ocean-pollution-statistics-facts#:~:text=An%20estimated%208%20Million%20tons,15%25%20lands%20on%20our%20beaches.
+// https://www.smithsonianmag.com/smart-news/the-us-recycled-just-5-percent-of-its-plastic-in-2021-180980052/
